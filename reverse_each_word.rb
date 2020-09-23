6e425d1adf72ce4aa2a3859c95826d98faddd568
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-23 12:23:25 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string) 
+  string_array = string.split
+  reversed_string = ""
+  string_array.each do |word| 
+    
+    reversed_string << word.reverse + " " #.join(" ")
+    
+    #reversed_string.each do |new_word|
+    # new_word.join(" ")
+end
+reversed_string.chop
+#reversed_string
+
+end
